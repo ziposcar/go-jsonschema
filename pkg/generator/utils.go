02e13a73a99b432a55636fdd5c6104a55798c826
liyichao <liyichao@bytedance.com>
2021-02-22 20:07:28 +0800

    feat: exchange import path

--- pkg/generator/utils.go
+++ pkg/generator/utils.go
@@ -6,8 +6,8 @@ import (
 	"sort"
 	"unicode"
 
-	"github.com/atombender/go-jsonschema/pkg/codegen"
-	"github.com/atombender/go-jsonschema/pkg/schemas"
+	"github.com/ziposcar/go-jsonschema/pkg/codegen"
+	"github.com/ziposcar/go-jsonschema/pkg/schemas"
 )
 
 func hashArrayOfValues(values []interface{}) string {
